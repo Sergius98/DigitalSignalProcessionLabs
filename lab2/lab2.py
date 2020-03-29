851b485db48c07322073a494c4df871014c6d107
sergius98 <sergei.mykytenko@gmail.com>
2020-03-29 14:04:25 +0300

    added comments

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -2,10 +2,31 @@ import librosa
 from ploting import plot1, plot_mfccs
 
 
-def show(filename="audio2.wav"):
+def read_audio(filename):
+
     input_audio, sample_rate = librosa.load(filename)
+
+    return input_audio, sample_rate
+
+
+def mfcc(input_audio, sample_rate, num_mfcc):
+
+    mfcc_sequence = librosa.feature.mfcc(y=input_audio, sr=sample_rate, n_mfcc=num_mfcc)
+
+    return mfcc_sequence
+
+
+def show(filename="audio2.wav"):
+    # read audio from file
+    input_audio, sample_rate = read_audio(filename)
+
+    # draw audio on a plot
     plot1(filename, input_audio, 'input audio')
-    mfcc_sequence = librosa.feature.mfcc(y=input_audio, sr=sample_rate, n_mfcc=12)
+
+    # make mfccs
+    mfcc_sequence = mfcc(input_audio, sample_rate, 12)
+
+    # draw mfcc on a plot
     plot_mfccs(mfcc_sequence, title='Mel-cepstrum coefficients',
                filename=filename)
     # mfcc_sequence = librosa.feature.mfcc(y=input_audio, sr=sample_rate, n_mfcc=40)
